b20dd39b3c3d759ac830e3740cc10bd998541e94
chebanroman-sigma <r3388967@gmail.com>
2024-12-23 18:15:48 +0200

    3
    3

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {/// <summary>
+     /// Задание 3. Вывести в столбик 5 раз слово «Привет!» (циклы с условием)
+     /// </summary>
+     /// <param name="args"></param>
+     /// 
+
+
+        //public static void Func()
+        //{
+
+        //    for (int i = 1; i <= 5; i++) 
+        //    { 
+        //    Console.WriteLine("Привет");
+        //    }
+        //}
+
+        ////////
+
+        //public static void Func()
+        //{
+        //    int i = 1;
+        //    while (i<=5)
+        //    {
+        //        Console.WriteLine("Привет");
+        //        i++;
+        //    }
+        //}
+
+        /////////
+
+        //public static void Func()
+        //{
+        //    int i = 1;
+        //    do
+        //    {
+        //        Console.WriteLine("Привет");
+        //        i++;
+        //    } while (i <= 5);
+        //}
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+    }
+}
